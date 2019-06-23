7b532e7e4bd68fee99649a5f6cb7c2795c140175
Richard Bock <richard.bock@rbock44.de>
2019-06-23 18:10:07 +0200

    FIX: project_launch wait until athene2 is ready with 200 OK

--- mk/tools.mk
+++ mk/tools.mk
@@ -9,10 +9,20 @@ tools_container_log_%:
 
 .PHONY: tools_dbsetup_log
 # show the athene2 content provider log
-tools_dbsetup_log: kubectl_use_context tools_container_log_dbsetup
+tools_dbsetup_log: kubectl_usecontext
 	kubectl logs $$(kubectl get pods --namespace kpi | grep dbsetup | awk '{ print $$1 }') --all-containers=true --namespace kpi --follow
 
 .PHONY: tools_athene2_app_log
 # show the httpd log
-tools_athene2_app_log: 
+tools_athene2_app_log: kubectl_use_context
 	kubectl logs $$(kubectl get pods --namespace athene2 | grep athene2-app | awk '{ print $$1 }') --all-containers=true --namespace athene2 --follow
+
+.PHONY: tools_editor_log
+# show the editor log
+tools_editor_log: kubectl_use_context
+	kubectl logs $$(kubectl get pods --namespace athene2 | grep ^editor-renderer-app | awk '{ print $$1 }') --namespace athene2 --follow
+
+.PHONY: tools_legacy_editor_log
+# show the editor log
+tools_legacy_editor_log: kubectl_use_context
+	kubectl logs $$(kubectl get pods --namespace athene2 | grep legacy-editor-renderer-app | awk '{ print $$1 }') --namespace athene2 --follow
