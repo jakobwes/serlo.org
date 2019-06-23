7ab43ba48eea3d09e09f9828aec883fb1f92c60f
Richard Bock <richard.bock@rbock44.de>
2019-06-23 14:06:44 +0200

    ADD: legacy-editor and editor

--- mk/minikube.mk
+++ mk/minikube.mk
@@ -10,23 +10,23 @@ project_start: minikube_start project_launch
 
 .PHONY: minikube_start
 minikube_start:
-	$(MAKE) -C $(infrastructure_repository)/minikube minikube_start
+	$(MAKE) -C $(infrastructure_repository)/minikube/atehen2 minikube_start
 
 .PHONY:
 minikube_create:
-	$(MAKE) -C $(infrastructure_repository)/minikube minikube_create
+	$(MAKE) -C $(infrastructure_repository)/minikube/athene2 minikube_create
 
 .PHONY:
 minikube_stop:
-	$(MAKE) -C $(infrastructure_repository)/minikube minikube_stop
+	$(MAKE) -C $(infrastructure_repository)/minikube/athene2 minikube_stop
 
 .PHONY:
 minikube_delete:
-	$(MAKE) -C $(infrastructure_repository)/minikube minikube_delete
+	$(MAKE) -C $(infrastructure_repository)/minikube/athene2 minikube_delete
 
 .PHONY:
 minikube_dashboard:
-	$(MAKE) -C $(infrastructure_repository)/minikube minikube_dashboard
+	$(MAKE) -C $(infrastructure_repository)/minikube/athene2 minikube_dashboard
 
 .PHONY:
 kubectl_use_context:
